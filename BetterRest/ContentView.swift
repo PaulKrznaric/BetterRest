62b18718c3f4824d76c6a2e267da897e187bef37
Paul Krznaric <paulkrznaric@gmail.com>
2020-06-13 21:15:17 -0400

    Updated to include Challenges 1-3, changed VStacks to Sections, removed the alert and added a constant bed time item on the bottom, changed the cups of coffee to a picker.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,17 +14,11 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
-    
     var body: some View {
         NavigationView {
             Form {
-                 VStack(alignment: .leading, spacing: 0){
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                Section(header: Text("When do you want to wake up?")                     .font(.headline))
+                {
                     
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
@@ -32,35 +26,37 @@ struct ContentView: View {
                         .datePickerStyle(WheelDatePickerStyle())
                 }
                 
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Desired amount of sleep")
-                        .font(.headline)
+                Section(header: Text("Desired amount of sleep")
+                .font(.headline)){
+                    
                     
                     Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
-                        Text("\(sleepAmount, specifier: "%g") cups")
+                        Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
                 
-                 VStack(alignment: .leading, spacing: 0){
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(value: $coffeeAmount, in: 1...20) {
-                        if coffeeAmount == 1 {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeeAmount) cups")
+                Section(){
+                    Picker("Daily coffee intake", selection: $coffeeAmount) {
+                        ForEach( 0..<21) {
+                            if $0 == 1 {
+                                Text("1 cup")
+                            } else {
+                                Text("\($0) cups")
+                            }
                         }
                     }
-                 }
-            .navigationBarTitle("BetterRest")
-                .navigationBarItems(trailing: Button(action: calculateBedTime){
-                    Text("Calculate")
                 }
-            )
-                    .alert(isPresented: $showingAlert) {
-                        Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("")))
+                    
+                Section(header: Text("Recommended Bed Time:") .font(.headline)
+                    .CenterText()){
+                    Text("\(calculatedBedTime)")
+                        .font(.largeTitle)
+                        .foregroundColor(Color(UIColor.systemGreen))
+                        .CenterText()
                 }
+                    
+                    
+                .navigationBarTitle("BetterRest")
                 
             }
         }
@@ -73,7 +69,7 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date()
     }
     
-    func calculateBedTime() {
+    var calculatedBedTime: String {
         let model = SleepCalculator()
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
@@ -88,15 +84,25 @@ struct ContentView: View {
             let formatter = DateFormatter()
             formatter.timeStyle = .short
             
-            alertMessage = formatter.string(from: sleepTime)
-            alertTitle = "Your ideal sleep time is..."
+            return formatter.string(from: sleepTime)
             
         } catch{
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime"
+            return "There was an error, please try again"
         }
-        
-        showingAlert = true
+    }
+}
+
+struct Center: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+    }
+}
+
+extension View {
+    func CenterText() -> some View {
+        self.modifier(Center())
     }
 }
 
